b6d9d2216b874c01719eff1bef3c6ff197b3f026
aridonguri <aridonguri@hotmail.com>
2017-07-15 14:15:40 -0700

    Fix

---
+++ AppiOS1.1/MapPin.m
@@ -0,0 +1,15 @@
+//
+//  MapPin.m
+//  AppiOS1.1
+//
+//  Created by Aridon on 7/15/17.
+//  Copyright © 2017 Aridon. All rights reserved.
+//
+
+#import "MapPin.h"
+
+@implementation MapPin
+
+@synthesize coordinate,title,subtitle;
+
+@end
